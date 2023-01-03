87efd6141b13e586d1ee539c8b11eae6c6dabbfe
Łukasz Holeczek <lukasz@holeczek.pl>
2023-01-03 20:46:47 +0100

    chore: clean-up

--- src/js/color-modes.js
+++ src/js/color-modes.js
@@ -1,10 +1,4 @@
 /* eslint-disable unicorn/no-array-for-each */
-/*!
- * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
- * Copyright 2011-2022 The Bootstrap Authors
- * Licensed under the Creative Commons Attribution 3.0 Unported License.
- */
-
 (() => {
   'use strict'
 
